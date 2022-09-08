a02e58125106a55dba96292acf6813d3e7a714eb
betty-alagwu <bettyalagwu@gmail.com>
2022-09-08 16:42:34 -0700

    writing a basic unix output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** 
+ * main - entry point
+ * Return - 1 (success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
