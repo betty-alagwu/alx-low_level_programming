7922556a938a183aa4dee67c352b87d52b678458
betty-alagwu <bettyalagwu@gmail.com>
2022-09-08 18:10:38 -0700

    new change

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 /** 
  * main - entry point
  * Return - 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
