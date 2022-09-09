43f75b326b5bbede3361c91801fab6516c01c464
betty-alagwu <bettyalagwu@gmail.com>
2022-09-09 13:02:58 -0700

    update positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 
 /**
  * main - Entry point
- *
+ * 
  * Description: 'running program to check if number is zero'
- *
+ * 
  * Return: Always 0 (Success)
  */
 
@@ -19,7 +19,7 @@ int main(void)
 
 	if (n > 0)
 		printf("%d is positive\n", n);
-       	else if (n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
