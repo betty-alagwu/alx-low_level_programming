bbb2b236e0b69813b52ab4056020e2d9d7c4ebda
betty-alagwu <bettyalagwu@gmail.com>
2022-09-08 18:46:12 -0700

    new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
  *
  * Description - C program description
  *
- * Return - Always 1 (success)
+ * Return: Always 1 (success)
  */
-
 int main(void)
 {
 	char chr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	fprintf(stderr, "%s", chr);
 	return (1);
 }
