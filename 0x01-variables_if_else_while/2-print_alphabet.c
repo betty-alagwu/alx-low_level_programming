1bdc69e67da39f8f152025ecfcc54688a107607e
betty-alagwu <bettyalagwu@gmail.com>
2022-09-09 16:36:58 -0700

    printing alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'running program to check if number is zero'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+		putchar('\n');
+	}
+	return (0);
+}
