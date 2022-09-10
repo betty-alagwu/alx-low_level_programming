e26f7f517c76039fed4bc8afb5ff135fa91c0937
betty-alagwu <bettyalagwu@gmail.com>
2022-09-09 18:22:39 -0700

    new fix

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - 'program to print lowercase aphabets'
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
- * Return - Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
