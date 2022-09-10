41e557087f5e6d9b173c2b92dc7084f5f4c6cbe2
betty-alagwu <bettyalagwu@gmail.com>
2022-09-09 17:32:40 -0700

    printing out the number from 0 - 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return - Always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
