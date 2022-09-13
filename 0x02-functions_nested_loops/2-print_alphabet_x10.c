e8a222df3f431d191dcc07141f3459bf27c6efad
betty-alagwu <bettyalagwu@gmail.com>
2022-09-13 10:30:08 -0700

    printing alphabets in small letters 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet in uppercase, followed by a new line.
+ * print_alphabet_x10 - prints the alphabet in uppercase 10 times.
  */
 void print_alphabet_x10(void)
 {
