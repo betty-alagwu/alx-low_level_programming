b8c5d96a87dfc3445038337d774ea9683681c507
betty-alagwu <bettyalagwu@gmail.com>
2022-09-13 11:39:35 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,14 @@
 /**
  * _isalpha - checks for alphabetic character.
  *
+ * @c: - checks the character
+ *
  * Return - 1 if c is a letter, else 0
  */
 int _isalpha(int c)
 {
-	if (((c >= 'a' && c <= 'z')) ||
-		(c >= 'A' && c <= 'Z'))
-			return (1);
-	else 
+	if (((c >= 'a' && c <= 'z')) || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
 		return (0);
 }
