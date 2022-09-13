966824385b55c236f7f0bc09af38bd56b3187d3b
betty-alagwu <bettyalagwu@gmail.com>
2022-09-13 06:55:27 -0700

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - prints "_putchar" followed by a new plan
  *
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * file - main.h
+ *
+ * description - header file containing declaration for all for all functions
+ */
+
 int _putchar(char ch);
 
 #endif
