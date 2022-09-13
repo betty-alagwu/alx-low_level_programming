6c2742b16fecc111a55731fba3941e8ed67544fc
betty-alagwu <bettyalagwu@gmail.com>
2022-09-13 13:04:41 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,9 @@
  *
  * @c: The number of which the sign will be printed.
  *
- * Return: 1 if the number is greater than zero, 0 if the number is zero, -1 if the number is less than zero.
+ * Return: 1 if the number is greater than zero, 
+ *         0 if the number is zero, 
+ *         -1 if the number is less than zero.
  */
 int print_sign(int n)
 {
@@ -23,6 +25,5 @@ int print_sign(int n)
 	{
 		_putchar('-');
 		return (-1);
-	
 	}
 }
