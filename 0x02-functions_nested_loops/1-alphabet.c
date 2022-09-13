d4d0dbd31fe27ecaa464260df788b745b929fce3
betty-alagwu <bettyalagwu@gmail.com>
2022-09-13 09:56:35 -0700

    printing alphabets in small letters

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
-/** 
+/**
  * main - entry point
  *
  * description - printing alphabets in small letters
  *
  * Return - Always 0.
  */
+
 void print_alphabet(void)
 {
 	char ch;
-	for(ch = 'a'; ch <= 'z'; ch++)
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
