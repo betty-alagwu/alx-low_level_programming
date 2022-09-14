096aee05db3b26d08862cb85c43490faf0fecb51
Ugly_Betty🙈 <bettyalagwu@gmail.com>
2022-09-14 16:49:57 +0100

    fix formatting

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,16 +5,21 @@
 /**
  * main - Entry point
  *
- * Description: 'running program to check if number is zero'
- *
  * Return: Always 0 (Success)
  */
-void positive_or_negative(int n)
+int positive_or_negative(void)
 {
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
-else
-printf("%d is zero\n", n);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+
+	return (0);
 }
