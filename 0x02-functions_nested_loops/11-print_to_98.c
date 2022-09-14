16b809f894f7c0f13eea193f42a3b5d5c937c43d
Ugly_Betty🙈 <bettyalagwu@gmail.com>
2022-09-14 03:27:57 +0100

    fix formatting

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,28 +9,29 @@
  *
  * Return: void.
  */
-void print_to_98(int n) {
-  if (n <= 98) {
-    int i;
-    for (i = n; i <= 98; i++) {
-      if (i == 98) {
-        printf("%d", i);
-      } else {
-        printf("%d, ", i);
-      }
+void print_to_98(int n)
+{
+if (n <= 98) {
+int i;
+for (i = n; i <= 98; i++) {
+    if (i == 98) {
+    printf("%d", i);
+    } else {
+    printf("%d, ", i);
     }
+}
 
-    printf("\n");
-  } else {
-    int i;
-    for (i = n; i >= 98; i--) {
-      if (i == 98) {
-        printf("%d", i);
-      } else {
-        printf("%d, ", i);
-      }
+printf("\n");
+} else {
+int i;
+for (i = n; i >= 98; i--) {
+    if (i == 98) {
+    printf("%d", i);
+    } else {
+    printf("%d, ", i);
     }
+}
 
-    printf("\n");
-  }
+printf("\n");
+}
 }
