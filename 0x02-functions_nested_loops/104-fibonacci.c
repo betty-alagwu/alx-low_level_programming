eb9e180baf3e13f2ec179bb3402932d373419f66
Ugly_Betty🙈 <bettyalagwu@gmail.com>
2022-09-14 04:50:26 +0100

    new fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -25,6 +25,7 @@ fib1_half1 = fib1 / 10000000000;
 fib2_half1 = fib2 / 10000000000;
 fib1_half2 = fib1 % 10000000000;
 fib2_half2 = fib2 % 10000000000;
+
 for (count = 93; count < 99; count++)
 {
 half1 = fib1_half1 + fib2_half1;
