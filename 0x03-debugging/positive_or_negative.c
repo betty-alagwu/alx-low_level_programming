ac2bebcd349b9d50d26e6d7d1ad4879358247a7d
Ugly_Betty🙈 <bettyalagwu@gmail.com>
2022-09-14 15:46:15 +0100

    add debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'running program to check if number is zero'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void positive_or_negative(int n)
+{
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is zero\n", n);
+}
