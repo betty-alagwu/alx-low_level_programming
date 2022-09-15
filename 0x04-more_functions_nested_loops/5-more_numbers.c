80d0685b5363de849d96511da15e7e5b635602c2
Ugly_Betty🙈 <bettyalagwu@gmail.com>
2022-09-15 20:19:40 +0100

    printing 10times a number from 0 - 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers from 0-9 except 2 amd 4.
+ */
+void more_numbers(void)
+{
+	int num, count;
+
+	for (count = 0; count <= 9; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
