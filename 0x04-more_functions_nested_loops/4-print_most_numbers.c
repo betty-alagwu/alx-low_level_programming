911c9b0dd35b225e6cb642a4da7010b5b99546f8
Ugly_Betty🙈 <bettyalagwu@gmail.com>
2022-09-15 19:59:53 +0100

    new commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,8 +10,8 @@ void print_most_numbers(void)
 	for (num = 0; num <= 9; num++)
 	{
 		if (num != 2 && num != 4)
-            _putchar(num + '0');
-    }
+			_putchar(num + '0');
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
