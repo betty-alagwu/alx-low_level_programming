09bc6a80cc09cca2512b3cb4f448341ad3f6ea59
Ugly_Betty🙈 <bettyalagwu@gmail.com>
2022-09-21 22:00:06 +0100

    encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,20 +9,39 @@
  */
 char *rot13(char *str)
 {
-	int i = 0;
-    int k = 0;
-    char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int indx1 = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
 
-	for (; str[i] != '\0'; i++)
+	while (str[indx1])
 	{
-		for (k = 0; k <= 51; k++)
+		for (indx2 = 0; indx2 < 52; indx2++)
 		{
-			if (s[k] == str[i])
+			if (str[indx1] == alphabet[indx2])
 			{
-				str[i] = s1[k];
+				str[indx1] = rot13key[indx2];
+				break;
 			}
 		}
+
+		indx1++;
 	}
+
 	return (str);
 }
