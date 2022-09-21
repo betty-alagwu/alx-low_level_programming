eb25c7fa05b6a201daf34dd054c287316dbf5575
Ugly_Betty🙈 <bettyalagwu@gmail.com>
2022-09-21 19:50:08 +0100

    coping a strting

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,12 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy str to dest
+ * @n: Number of bytes to use max
+ * @dest: String to copied to
+ * @src: String to be copied to dest
+ * Return: Always 0.
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
