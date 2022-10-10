e9bd24c03327794e9d93fc6e64d3b7dbe8c57e5a
Ugly_Betty🙈 <bettyalagwu@gmail.com>
2022-10-10 13:42:57 +0100

    completed task

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
  *
- * Return: 0
+ * Return: Always 0.
  */
 int main(void)
 {
-    printf("%s\n", __BASE_FILE__);
-    return 0;
+	printf("%s\n", __FILE__);
+
+	return (0);
 }
