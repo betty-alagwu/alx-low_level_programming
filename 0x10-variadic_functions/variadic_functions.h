a2c557846c6515607a8e1fe7f6af2d9b67bf2152
Ugly_Betty🙈 <bettyalagwu@gmail.com>
2022-10-13 11:22:34 +0100

    completed task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
