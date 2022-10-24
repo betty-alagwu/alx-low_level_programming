9f26c7c53d9012856fcc3ec00383e2c25d75af47
Betty Alagwu <bettyalagwu@gmail.com>
2022-10-25 00:19:14 +0100

    completed task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,21 +2,21 @@
 #include "lists.h"
 
 /**
- * print_listint - Print out the integer contained in a listint_t node
+ * print_listint: Print out the integer contained in a listint_t node
  * @h: Pointer to the start of the list
  *
  * Return: Number of nodes in the list
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodecount;
+size_t nodecount;
 
-	nodecount = 0;
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		nodecount++;
-		h = h->next;
-	}
-	return (nodecount);
+nodecount = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+nodecount++;
+h = h->next;
+}
+return (nodecount);
 }
