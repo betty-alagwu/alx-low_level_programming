69469280946dd5096df5459190f9df40a4b32df0
Betty Alagwu <bettyalagwu@gmail.com>
2022-10-25 00:20:49 +0100

    completed task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t nodecount;
+	size_t nodecount;
 
-nodecount = 0;
-while (h != NULL)
-{
-printf("%d\n", h->n);
-nodecount++;
-h = h->next;
-}
-return (nodecount);
+	nodecount = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		nodecount++;
+		h = h->next;
+	}
+	return (nodecount);
 }
